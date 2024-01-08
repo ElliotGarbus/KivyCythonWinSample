5a4b485d0a4833361886bc7d4d9ee7bf78d28cf9
ElliotGarbus <elliotg2@cox.net>
2024-01-07 17:56:21 -0700

    minor edits

--- myapplication/configstartup.py
+++ myapplication/configstartup.py
@@ -1,6 +1,6 @@
 from kivy.config import Config
-from kivy.utils import platform
-import os
+
+# import os
 
 # os.environ["KIVY_NO_FILELOG"] = "1"
 # os.environ["KIVY_NO_CONSOLELOG"] = "1"
@@ -11,7 +11,6 @@ configstartup.py is for initial setup of the environment
 This file MUST be imported at the top of the 'main' executable file.  
  
 """
-
 
 Config.set('kivy', 'window_icon', 'asset/cropped cactus 64x64.png')  # Windows uses a 64x64 png
 Config.set('kivy', 'exit_on_escape', 0)
--- myservice/main.py
+++ myservice/main.py
@@ -33,7 +33,7 @@ class MyService:
         secret = ProprietaryClass()
         while self.running:
             time.sleep(10)  # Important work
-            secret_result = secret.hellow_world()
+            secret_result = secret.hello_world()
             servicemanager.LogInfoMsg(f"{secret_result} {MyLib_Constants}")
 
 class MyServiceFramework(win32serviceutil.ServiceFramework):
@@ -48,7 +48,7 @@ class MyServiceFramework(win32serviceutil.ServiceFramework):
         self.ReportServiceStatus(win32service.SERVICE_STOPPED)
 
     def SvcDoRun(self):
-      """Start the service; does not return until stopped"""
+        """Start the service; does not return until stopped"""
         self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
         self.service_impl = MyService()
         self.ReportServiceStatus(win32service.SERVICE_RUNNING)
