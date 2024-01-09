bbcffe50e2e2b687d4cbe8139b4a112814af5e78
ElliotGarbus <elliotg2@cox.net>
2024-01-08 19:48:26 -0700

    comment added

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
+# from the command line at the project level type:
+# > python setup.py build_ext --inplace
+# to build the cython files to object files
+
 setup(
     name='My Library',
     ext_modules=cythonize("mylibrary/**/*.pyx"),
