e9970003e530855ae1393507f02c9c3af1a84ecb
ElliotGarbus <elliotg2@cox.net>
2024-01-10 16:29:53 -0700

    c compiler flags added

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup
+from setuptools import Extension, setup
 from Cython.Build import cythonize
 
 # from the command line at the project level type:
 # > python setup.py build_ext --inplace
 # to build the cython files to object files
 
-# todo: set c compiler flags
+extensions = [Extension(name='mylibrary',
+                        sources=["mylibrary/**/*.pyx"],
+                        extra_compile_args=['/O2'])]
 
 setup(
     name='My Library',
-    ext_modules=cythonize("mylibrary/**/*.pyx"),
+    ext_modules=cythonize(extensions)
 )
