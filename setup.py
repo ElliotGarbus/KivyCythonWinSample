faf6f094a801ffe3a79232f4fd66578f277eac16
ElliotGarbus <elliotg2@cox.net>
2024-01-17 16:27:28 -0700

    moved __init__ back to .py, updated setup.py for cython builds

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
-from setuptools import Extension, setup
+from setuptools import Extension, setup, find_packages
 from Cython.Build import cythonize
 
 # from the command line at the project level type:
 # > python setup.py build_ext --inplace
 # to build the cython files to object files
 
-extensions = [Extension(name='mylibrary',
-                        sources=["mylibrary/**/*.pyx"],
-                        extra_compile_args=['/O2'])]
+extensions = [Extension(name='mylibrary.mylibrary',
+                        sources=['mylibrary/*.pyx'],
+                        extra_compile_args=['/O2']),
+              Extension(name='mylibrary.subdir.constants',
+                        sources=['mylibrary/subdir/*.pyx'],
+                        extra_compile_args=['/O2']),
+              ]
+
+cython_directives = {'embedsignature': True}
 
 setup(
-    name='My Library',
-    ext_modules=cythonize(extensions)
+    name='mylibrary',
+    version='0.0.0',  # todo: replace with meta data
+    zip_safe=False,  # Without these two options
+    include_package_data=True,  # PyInstaller may not find your C-Extensions
+    ext_modules=cythonize(extensions, compiler_directives=cython_directives, language_level='3')
 )
